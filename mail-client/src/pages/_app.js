e9874214b62246c0551d8adcb18b02cd552e5abe
qbentil <bentilshadrack72@gmail.com>
2023-04-23 10:38:24 +0000

    Implement Demo

--- mail-client/src/pages/_app.js
+++ mail-client/src/pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
+
+import { ToastContainer } from "react-toastify";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ToastContainer autoClose={3000} hideProgressBar />
+      <Component {...pageProps} />
+    </>
+  );
 }
