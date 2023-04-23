a1ad4c71a82f493c93b8897076067d6b097aa873
qbentil <bentilshadrack72@gmail.com>
2023-04-23 10:08:56 +0000

    Fix CORS issue

--- server.ts
+++ server.ts
@@ -2,11 +2,12 @@ import ErrorHandler from './middlewares/errorHandler';
 import SEND_ROUTE from './routes/send.routes';
 import dotenv from 'dotenv';
 import express from 'express';
-
+import cors from "cors";
 dotenv.config()
 const PORT = process.env.PORT || 8000
 const APP = express()
 APP.use(express.json())
+APP.use(cors({credentials: true, origin:true}));
 APP.use("/api/send", SEND_ROUTE)
 
 APP.use(ErrorHandler)
